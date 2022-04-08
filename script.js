297f2de391872c4c86b1b576e7b320cb1b60426b
unknown <fanglu0805@gmail.com>
2022-04-08 14:07:18 -0400

    just need to add date

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-//api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={aa9307862b9abd3a9361e261c9b422d6}
+let weather = {
+    "apiKey": "3b81ebede925f70faef548f7e16c5982", 
+    fetchWeather: function (zip) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&appid=${this.apiKey}`
+            )
+            .then((response) => response.json()) 
+            .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function(data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, feels_like, temp_max, temp_min} = data.main;
+        const {speed} = data.wind;
+        console.log(name,icon,description,temp,feels_like,temp_min,temp_max,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon +".png";
+        document.querySelector(".desc").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".feelsLike").innerText = "Feels like: " + feels_like;
+        document.querySelector(".wind").innerText = "Wind Speed is: " + speed + " mph";
+        document.querySelector(".low").innerText = "Low of: " + temp_min;
+        document.querySelector(".high").innerText = "High of: " + temp_max;
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".searchButn").value);
+    }
+}
+
+document.querySelector(".search button").addEventListener("click", function() {
+weather.search();
+});
+
+// document.querySelector(".city").innerText = "Weather in " + name;
+// document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+// document.querySelector(".description").innerText = description;
+// document.querySelector("temp").innerText = temp + "°F";
+// document.querySelector(".feels_like").innerText = "Feels like: " + feels_like;
+// document.querySelector(".speed").innerText = "Wind Speed is: " + speed + " mph";
+// document.querySelector(".temp_min").innerText = "Low of: " + temp_min;
+// document.querySelector(".temp_max").innerText = "High of: " + temp_max;
